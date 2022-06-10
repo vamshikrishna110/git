36dc8929490c9883f959224c7b60a5b1c2b643fe
vamshikrishna <kvamshikrishna2016@gmail.com>
2022-06-10 17:59:27 +0530

    counter

--- index.js
+++ index.js
@@ -1,74 +1,46 @@
-let containerE1 = document.createElement("div");
-document.body.appendChild(containerE1);
+let divE1 = document.createElement("div");
+document.body.appendChild(divE1)
 
 let paraE1 = document.createElement("p");
-paraE1.textContent = "Name";
-containerE1.appendChild(paraE1);
-let inputE1 = document.createElement("input");
-inputE1.type = "text";
-containerE1.appendChild(inputE1);
+paraE1.textContent = "0";
+paraE1.id = "counterValue";
+paraE1.classList.add("counter-value");
+document.body.appendChild(paraE1);
 
-let paraE2 = document.createElement("p");
-paraE2.textContent = "address";
-containerE1.appendChild(paraE2)
-let inputE2 = document.createElement("input")
-inputE2.type = "text"
-containerE1.appendChild(inputE2)
+let btnE2 = document.createElement("button");
+btnE2.textContent = "decrease";
+btnE2.classList.add("btnEle");
+document.body.appendChild(btnE2);
 
-let paraE3 = document.createElement("p");
-paraE3.textContent = "door no";
-containerE1.appendChild(paraE3);
-let inputE3 = document.createElement("input")
-inputE3.type = "text"
-containerE1.appendChild(inputE3)
+let btnE1 = document.createElement("button");
+btnE1.textContent = "increase";
+btnE1.classList.add("btnEle");
+document.body.appendChild(btnE1);
 
-let breakE1 = document.createElement("br")
-document.body.appendChild(breakE1)
-
-let checkboxE1 = document.createElement("input");
-checkboxE1.type = "checkbox";
-checkboxE1.id = "mycheckbox"
-document.body.appendChild(checkboxE1)
-
-let breakE2 = document.createElement("br")
-document.body.appendChild(breakE2)
-
-
-
-
-let containerE2 = document.createElement("div");
-document.body.appendChild(containerE2);
-
-let paraE11 = document.createElement("p");
-paraE11.textContent = "Name";
-containerE2.appendChild(paraE11);
-let inputE11 = document.createElement("input");
-inputE11.type = "text";
-containerE2.appendChild(inputE11);
-
-let paraE22 = document.createElement("p");
-paraE22.textContent = "address";
-containerE2.appendChild(paraE22)
-let inputE22 = document.createElement("input")
-inputE22.type = "text"
-containerE2.appendChild(inputE22)
-
-let paraE33 = document.createElement("p");
-paraE33.textContent = "door no";
-containerE2.appendChild(paraE33);
-let inputE33 = document.createElement("input")
-inputE33.type = "text"
-containerE2.appendChild(inputE33)
-
-
-checkboxE1.onclick = function() {
-    if (checkboxE1.checked == true) {
-        inputE11.value = inputE1.value
-        inputE22.value = inputE2.value
-        inputE33.value = inputE3.value
+btnE1.onclick = function() {
+    let previousCounterValue = paraE1.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) + 1;
+    if (updatedCounterValue > 0) {
+        paraE1.style.backgroundColor = "green";
+    } else if (updatedCounterValue < 0) {
+        paraE1.style.backgroundColor = "red";
     } else {
-        inputE11.value = ""
-        inputE22.value = ""
-        inputE33.value = ""
+        paraE1.style.backgroundColor = "yellow";
     }
-}
+    paraE1.textContent = updatedCounterValue;
+
+};
+
+btnE2.onclick = function onDecrement() {
+    let previousCounterValue = paraE1.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) - 1;
+    if (updatedCounterValue > 0) {
+        paraE1.style.backgroundColor = "green";
+    } else if (updatedCounterValue < 0) {
+        paraE1.style.backgroundColor = "red";
+    } else {
+        paraE1.style.backgroundColor = "yellow";
+    }
+    paraE1.textContent = updatedCounterValue;
+
+};
